b16fb5d4ddb7119681a90c2fbe3972010dc718d5
Jackson Davis <jdavis3695@gmail.com>
2022-02-23 14:25:19 -0500

    Trees

--- Solutions/Solved/Trees/postOrderTraversal.java
+++ Solutions/Solved/Trees/postOrderTraversal.java
@@ -1,20 +1,8 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+package Trees;
+
 import java.util.ArrayList;
 import java.util.List;
+
 class Main{
 
 
--- Solutions/Solved/Trees/preOrderTraversal.java
+++ Solutions/Solved/Trees/preOrderTraversal.java
@@ -1,22 +1,9 @@
-package Jackson.Java;
+package Trees;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
