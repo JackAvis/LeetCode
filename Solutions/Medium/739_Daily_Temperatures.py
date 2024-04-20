77d5de800f836a00a7e892ac460161b4880b4d8b
Jackson Davis <jdavis3695@gmail.com>
2024-04-20 12:24:36 -0700

    Update 739_Daily_Temperatures.py

--- Solutions/Medium/739_Daily_Temperatures.py
+++ Solutions/Medium/739_Daily_Temperatures.py
@@ -1,17 +1,13 @@
+from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # looked at solution
+        res = [0] * len(temperatures)
         stack = []
-        answers = [0 for i in range(len(temperatures))]
-        for i, temp in enumerate(temperatures):
-            while stack and stack[-1][0] < temp:
-                topOfStack = stack.pop()
-                indexCalc = i - topOfStack[1]
-                answers[topOfStack[1]] = indexCalc
-            stack.append((temp, i))
-        return answers
-
-
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                index_on_stack = stack.pop()
+                res[index_on_stack] = i - index_on_stack
+            stack.append(i)
 
 
 
